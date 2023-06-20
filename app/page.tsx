bd82202997396cb28a8a1ac7d4c71b05d39b64ec
Fabrice <mwanafunzifabrice@gmail.com>
2023-06-20 20:23:59 +0200

    first boilerplate changes

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl max-auto mt-4">
+    <div className='text-center my-5 flex flex-col gao-4'>
+      <h1 className='text-4xl font-bold'>Todo App</h1>
+      <AddTask />
+    </div>
+    </main>
+  )
+}
