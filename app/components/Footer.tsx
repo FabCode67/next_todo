413fe994f6bdcf56c69e1e2c6efdd513838790af
Fabrice <mwanafunzifabrice@gmail.com>
2023-06-21 16:29:39 +0200

    final commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='b-0 mt'>
+    <p className="text-sm text-gray-500 flex justify-center space-x-3">&copy; {new Date().getFullYear()} <p>mwanafunzifabrice@gmail.com. </p> <p></p>All rights reserved.</p>
+      </footer>  )
+}
+
+export default Footer
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
 import { getAllTodos } from "@/api";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
+import Footer from "./components/Footer";
 
 export default async function Home() {
-  const tasks = await getAllTodos();  
+  const tasks = await getAllTodos();
+
   return (
-    <main className="flex flex-col max-w-4xl max-auto mt-4 justify-center m-auto">
-      <div className='text-center items-center my-5 flex flex-col gap-4'>
-        <h1 className='text-2xl font-bold'>Todo App</h1>
-        <AddTask />
-      </div>
-      <TodoList tasks={tasks} />
-    </main>
+    <div className="flex flex-col min-h-screen">
+      <main className="flex flex-col max-w-4xl max-auto mt-4 justify-center m-auto w-[70%]">
+        <div className='text-center items-center my-5 flex flex-col gap-4'>
+          <h1 className='text-2xl font-bold'>Todo App</h1>
+          <AddTask />
+        </div>
+        <TodoList tasks={tasks} />
+      </main>
+      <footer className="bg-gray-200 py-4 text-center">
+       <Footer />
+      </footer>
+    </div>
   );
 }
