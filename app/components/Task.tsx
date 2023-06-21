070382959cb1f679fdb8dd5cbf7a4e87ba75ac19
Fabrice <mwanafunzifabrice@gmail.com>
2023-06-21 15:13:17 +0200

    pushing fully one

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,19 +1,22 @@
 'use client'
-
-import { ITask } from '@/types/tasks'
-import { updateChacked } from '@/api'
-import { deleteTodo } from '@/api';
+import { ITask } from '@/types/tasks';
+import { updateChacked, handleUpdateTodo, deleteTodo } from '@/api';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { AiOutlineEdit } from 'react-icons/ai'
-import { RiDeleteBin6Line } from 'react-icons/ri'
+import { AiOutlineEdit } from 'react-icons/ai';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import Model from './Model';
+
 interface TaskProps {
-  task: ITask
+  task: ITask;
 }
 
 const Task: React.FC<TaskProps> = ({ task }) => {
   const route = useRouter();
   const [checked, setChecked] = useState(task.checked);
+  const [editMode, setEditMode] = useState(false);
+  const [updatedText, setUpdatedText] = useState(task.text);
+
   const handleCheckboxChange = async () => {
     const updatedTask = { ...task, checked: !checked };
     setChecked(!checked);
@@ -21,11 +24,26 @@ const Task: React.FC<TaskProps> = ({ task }) => {
     route.refresh();
   };
 
-  const handleDelete = async(id:string) => {
-   await deleteTodo(id)
+  const handleDelete = async (id: string) => {
+    await deleteTodo(id);
     route.refresh();
-  }
+  };
 
+  const handleEdit = () => {
+    setEditMode(true);
+  };
+
+  const handleUpdate = async () => {
+    const updatedTask = { ...task, text: updatedText };
+    await handleUpdateTodo(updatedTask);
+    setEditMode(false);
+    route.refresh();
+  };
+
+  const handleCancelEdit = () => {
+    setEditMode(false);
+    setUpdatedText(task.text);
+  };
 
   return (
     <tr key={task.id}>
@@ -37,12 +55,40 @@ const Task: React.FC<TaskProps> = ({ task }) => {
           onChange={handleCheckboxChange}
         />
       </td>
-      <td className={`border-b-2 p-3 text-left border-gray-200 ${task.checked ? 'line-through':''}`}>{task.text}</td>
+      <td className={`border-b-2 p-3 text-left border-gray-200 ${task.checked ? 'line-through' : ''}`}>
+        {editMode ? (
+          <input
+            type='text'
+            className='w-full border-2 border-gray-200 rounded-md p-2'
+            value={updatedText}
+            onChange={(e) => setUpdatedText(e.target.value)}
+          />
+        ) : (
+          task.text
+        )}
+      </td>
       <td className='border-b-2 p-3 text-left border-gray-200  space-x-4'>
-        <button className='font text-2xl text-blue-500'><AiOutlineEdit /></button>
-        {task.checked &&
-        <button className='font text-2xl text-red-600' onClick={()=> handleDelete(task.id)}><RiDeleteBin6Line /></button>
-}
+        {editMode ? (
+          <>
+            <button type='submit' className='font text-2xl text-blue-500' onClick={handleUpdate}>
+              Save
+            </button>
+            <button className='font text-2xl text-gray-500' onClick={handleCancelEdit}>
+              Cancel
+            </button>
+          </>
+        ) : (
+          <>
+            <button className='font text-2xl text-blue-500' onClick={handleEdit}>
+              <AiOutlineEdit />
+            </button>
+            {task.checked && (
+              <button className='font text-2xl text-red-600' onClick={() => handleDelete(task.id)}>
+                <RiDeleteBin6Line />
+              </button>
+            )}
+          </>
+        )}
       </td>
     </tr>
   );
